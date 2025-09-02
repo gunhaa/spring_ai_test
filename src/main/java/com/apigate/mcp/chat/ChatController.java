fcdb427e3e1a2c465dd01082ba4e2926febeec1d
gunha <wh8299@gmail.com>
2025-09-02 10:47:07 +0900

    init, llama 연결 후 테스트까지 진행

---
+++ src/main/java/com/apigate/mcp/chat/ChatController.java
@@ -0,0 +1,31 @@
+package com.apigate.mcp.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final OllamaChatModel chatModel;
+
+    @GetMapping("/test")
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatModel.call(message));
+    }
+
+    @GetMapping("/streamTest")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatModel.stream(prompt);
+    }
+}
---
+++ src/test/java/com/apigate/mcp/McpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigate.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
